122fb6cb0044ed30c2328aa915fec09364295dbf
edwinyoon <ep160101@gmail.com>
2018-12-06 12:12:01 -0500

    Add Burget menu markup

--- src/containers/Header/HeaderContainer/HeaderContainer.js
+++ src/containers/Header/HeaderContainer/HeaderContainer.js
@@ -34,29 +34,75 @@ const HeaderNavContainer = styled.div`
   justify-content: space-between;
   align-items: center;
 `;
-const TestDiv = styled.div`
-  display: block;
-  color: white;
-  margin-right: 10px;
+
+const BurgerMenu = styled.button`
+  margin-right: 30px;
+  width: 40px;
+  height: 40px;
+  /* background-color: black; */
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 5px 5px;
+  transition: all 0.2s ease;
+
+  /* span:nth-child(2) {
+    -webkit-transform: rotate(45deg);
+    -moz-transform: rotate(45deg);
+    -o-transform: rotate(45deg);
+    transform: rotate(45deg);
+  }
+  span:nth-child(3) {
+    -webkit-transform: rotate(-45deg);
+    -moz-transform: rotate(-45deg);
+    -o-transform: rotate(-45deg);
+    transform: rotate(-45deg);
+  } */
+  .bars {
+    width: 40px;
+    height: 4px;
+    /* border: 6px white solid; */
+    background-color: white;
+    border-radius: 5px;
+    z-index: 9999;
+  }
+  &:hover {
+    cursor: pointer;
+    .bm-bar_1 {
+      transform: translate(0px, -4px);
+      transition: all 0.2s linear;
+    }
+
+    .bm-bar_3 {
+      transform: translate(0px, 4px);
+      transition: all 0.2s linear;
+    }
+  }
   @media screen and (min-width: 960px) {
     display: none;
   }
 `;
 
 const HeaderContainer = ({navItems}) => (
-    <Container>
-      <BriefSection>
-        <LogoWrapper />
-      </BriefSection>
-      <DetailSection>
-        <SortByNav />
-        <HeaderNavContainer>
-          <HeaderNav navItems={navItems} />
-          <HeaderAPIButton />
-        </HeaderNavContainer>
-      </DetailSection>
-    </Container>
-  );
+  <Container>
+    <BriefSection>
+      <LogoWrapper />
+    </BriefSection>
+    <DetailSection>
+      <SortByNav />
+      <HeaderNavContainer>
+        <HeaderNav navItems={navItems} />
+        <HeaderAPIButton />
+      </HeaderNavContainer>
+    </DetailSection>
+    <BurgerMenu>
+      <span className="bars bm-bar_1" />
+      <span className="bars bm-bar_2" />
+      <span className="bars bm-bar_3" />
+    </BurgerMenu>
+  </Container>
+);
 
 export default HeaderContainer;
 HeaderContainer.displayName = 'HeaderContainer';
