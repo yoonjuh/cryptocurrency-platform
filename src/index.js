b919ce46b46d3f9fb89441f37973099e8aa18645
edwinyoon <ep160101@gmail.com>
2018-12-11 00:19:24 -0500

    Connect app with store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './global.css'
+import './global.css';
+import {Provider} from 'react-redux';
+import store from './store';
 import App from './routes/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
