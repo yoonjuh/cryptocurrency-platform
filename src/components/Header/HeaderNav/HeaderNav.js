97887cb0344858e2c3725881b8de9be9497c2439
edwinyoon <ep160101@gmail.com>
2018-12-11 00:18:02 -0500

    Remove unnecessary files

--- src/components/Header/HeaderNav/HeaderNav.js
+++ src/components/Header/HeaderNav/HeaderNav.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import HeaderNavItem from '../HeaderNavItem';
-import ResponsiveMenuBar from '../ResponsiveMenuBar';
 import withNavItems from '../../../StaticData/withNavItems';
 
 const Container = styled.div`
@@ -17,7 +16,6 @@ const HeaderNav = ({sortedNavItems}) => (
       sortedNavItems.map(({name, subNav}) => (
         <HeaderNavItem key={name} title={name} subNav={subNav} />
       ))}
-    <ResponsiveMenuBar />
   </Container>
 );
 export default withNavItems(HeaderNav);
