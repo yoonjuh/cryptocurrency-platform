dc12ff92628f0368ba453072587e79ffcd5ae563
edwinyoon <ep160101@gmail.com>
2018-12-17 22:26:33 -0500

    Add api calls for dashboard and price

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,37 @@
 import axios from 'axios';
 
-const {REACT_APP_API_BASE_URL, REACT_APP_API_KEY} = process.env;
+const {
+  REACT_APP_API_BASE_CURRENCY,
+  REACT_APP_API_KEY,
+  REACT_APP_API_MARKET_CAP,
+  REACT_APP_API_BASE_DASHBOARD,
+  REACT_APP_API_BASE_PRICE,
+} = process.env;
 
 export async function getAllCurrencies() {
-  console.log(process.env.REACT_APP_API_KEY);
-  const res = await axios.get(`${REACT_APP_API_BASE_URL}${REACT_APP_API_KEY}`);
-  console.log(JSON.stringify(res));
+  const res = await axios.get(
+    `${REACT_APP_API_BASE_CURRENCY}${REACT_APP_API_KEY}`
+  );
+}
+
+export async function getTotalMarketCap(start) {
+  const res = await axios.get(
+    `${REACT_APP_API_MARKET_CAP}${REACT_APP_API_KEY}&start=${'2018-12-15'}`
+  );
+
+  // console.log(res);
+}
+export async function getDashboard() {
+  const res = await axios.get(
+    `${REACT_APP_API_BASE_DASHBOARD}${REACT_APP_API_KEY}`
+  );
+
+  return res.data;
+}
+export async function getPrice() {
+  const res = await axios.get(
+    `${REACT_APP_API_BASE_PRICE}${REACT_APP_API_KEY}`
+  );
+
+  return res.data;
 }
