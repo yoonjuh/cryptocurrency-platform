a32c06b60198712c770dd20a58595180cc6454c4
edwinyoon <ep160101@gmail.com>
2018-12-18 02:57:35 -0500

    Add money format methods

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,4 @@
-import {reduce, find, propEq, merge} from 'ramda';
+import {reduce, find, propEq, merge, toString, split} from 'ramda';
 
 export const arrayConcater = (arr1, arr2) => {
   const merged = reduce((array, item) => {
@@ -8,4 +8,37 @@ export const arrayConcater = (arr1, arr2) => {
   }, [])(arr1);
 
   return merged;
+};
+export const moneyPrettier = (num, digit) =>
+  num.toLocaleString('en', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: digit,
+    maximumFractionDigits: digit,
+  });
+
+export const numberPrettier = num => {
+  if (isNaN(num)) return num;
+  const checker = split('.', toString(num))[0];
+
+  if (checker.length > 6) return moneyPrettier(parseInt(num), 0);
+  if (checker.length >= 3) return moneyPrettier(parseFloat(num), 2);
+
+  return moneyPrettier(parseFloat(num), 5);
+};
+
+export const supplyPrettier = num => {
+  if (isNaN(num)) return 'UNKNOWN';
+
+  return split(
+    '$',
+    toString(
+      num.toLocaleString('en', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0,
+      })
+    )
+  )[1].slice(0, -1);
 };
