935023e39bfce6fa2f1f89abe94645edd787abf6
edwinyoon <ep160101@gmail.com>
2018-12-19 14:18:54 -0500

    Add Number formatter for Change and ATH

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -9,6 +9,19 @@ export const arrayConcater = (arr1, arr2) => {
 
   return merged;
 };
+
+export const athConcater = (arr1, arr2) => {
+  const merged = reduce((array, item) => {
+    const x = find(propEq('currency', item.currency))(arr2);
+    // console.log(x);
+
+    return x ? [...array, merge(item, {ath: x})] : array;
+    // return [];
+  }, [])(arr1);
+
+  return merged;
+};
+
 export const moneyPrettier = (num, digit) =>
   num.toLocaleString('en', {
     style: 'currency',
@@ -41,4 +54,23 @@ export const supplyPrettier = num => {
       })
     )
   )[1].slice(0, -1);
+};
+
+export const changePrettier = num => {
+  if (isNaN(num)) return num;
+
+  const decimal = parseFloat(num).toFixed(2);
+  if (decimal >= 100.0) return '100.00%';
+  if (decimal <= -100.0) return '-100.00%';
+
+  return `${decimal}%`;
+};
+
+export const athPrettier = (ch, ath) => {
+  if (isNaN(ch) || isNaN(ath)) return '-';
+
+  const percentage = (parseFloat(ch) / parseFloat(ath)) * 100.0;
+  if (percentage < 1.0) return '<1%';
+
+  return `${parseInt(percentage)}%`;
 };
