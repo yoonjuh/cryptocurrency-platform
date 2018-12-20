abff062ce9d5bfecf9fdbdd00246e532423a3ea0
edwinyoon <ep160101@gmail.com>
2018-12-20 03:03:43 -0500

    Connect with real data && foratted nicely

--- src/components/Main/ContentItem/ContentItem.js
+++ src/components/Main/ContentItem/ContentItem.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import {
+  numberPrettier,
+  changePrettier,
+  athPrettier,
+} from '../../../utils/helpers';
+import {
   Index,
   Name,
   MarketCap,
@@ -8,32 +13,86 @@ import {
   Change,
   ATH,
   Volume,
-  Supply,
   PriceGraph,
+  InfoIcon,
 } from '../ColumeLayoutCommon/ColumnLayoutCommon';
+import SupplySummary from '../SupplySummary';
+import SupplyContent from '../SupplyContent';
 
 const ItemWrapper = styled.div`
-  width: 100%;
+  max-width: 100%; /** LAYOUT */
+  min-width: 100%;
   padding: 10px 15px;
+  line-height: 1.2;
 
-  font-size: 1rem;
+  display: flex; /** CHILD */
+
+  font-size: 1rem; /** FONT */
   font-family: 'Roboto Slap', sans-serif;
 
+  color: #484848; /** EFFECT */
   border-bottom: 0.2px grey solid;
-  display: flex;
+  letter-spacing: 2px;
 `;
 
-const ContentsItem = ({item, index}) => (
+const InfoIconWrapper = styled.div`
+  margin: 0px 5px;
+  color: #bbbbbb;
+
+  &:hover {
+    color: black;
+    transition: color 0.3s ease;
+
+    /* &.hoverable-summary {
+      + span {
+      opacity: 1;
+      transition: all 0.2s ease-in;
+      visibility: visible;
+    } */
+  }
+`;
+
+const ContentsItem = ({
+  item: {
+    currency,
+    price,
+    availableSupply,
+    maxSupply,
+    open,
+    openVolume,
+    volume,
+    close,
+    ath,
+  },
+  index,
+}) => (
   <ItemWrapper>
     <Index>{index}</Index>
-    <Name styles={{fontSize: '1.2rem', fontWeight: 500}}>{item.currency}</Name>
-    <MarketCap>{item.price}</MarketCap>
-    <Price>{item.price}</Price>
-    <Change>Changes</Change>
-    <ATH>ATH</ATH>
-    <Volume>{item.dayVolume}</Volume>
-    <Supply>Supply</Supply>
-    <PriceGraph>Graph</PriceGraph>
+    <Name styles={{fontSize: '1.2rem', fontWeight: 500}}>{currency}</Name>
+    <MarketCap styles={{cursor: 'auto'}}>
+      {numberPrettier(price * availableSupply)}
+    </MarketCap>
+    <Price styles={{cursor: 'auto'}}>{numberPrettier(price)}</Price>
+    <Change
+      stules={{cursor: 'auto'}}
+      clsName={
+        parseFloat(changePrettier(((open - close) / close) * 100)) <= 0.0
+          ? 'negative'
+          : 'positive'
+      }
+    >
+      {changePrettier(((open - close) / close) * 100)}
+    </Change>
+    <ATH styles={{cursor: 'auto'}}>
+      <div>{athPrettier(price, ath.price)}</div>
+      <InfoIconWrapper className="fas fa-info-circle" />
+    </ATH>
+    <Volume styles={{cursor: 'auto'}}>{numberPrettier(volume)}</Volume>
+    <SupplyContent availableSupply={availableSupply} maxSupply={maxSupply} />
+    <PriceGraph>
+      PriceChart
+      {/* <PriceChart /> */}
+    </PriceGraph>
   </ItemWrapper>
 );
 export default ContentsItem;
